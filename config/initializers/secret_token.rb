1084f6b0cbd040ce39c90d147917a63ded5f1606
jaipratik <jp_ca@live.com>
2011-11-05 10:35:51 -0700

    add more files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BandomeProject::Application.config.secret_token = '977981309a033a86c449db3b56f47d528393122a8774ea009bc19b31c6fadef531559a17c031550797df4e6c8ca9dc31610055afedb4ff769b772a6129941809'
